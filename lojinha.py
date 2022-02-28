75675ac4d31928503b158dca72b6c9b24450e078
Matheus Araujo <maatheus_98@hotmail.com>
2022-02-28 11:52:10 -0300

    Variable name to english

--- lojinha.py
+++ lojinha.py
@@ -15,7 +15,6 @@ from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTi
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
